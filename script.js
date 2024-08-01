367c5b7c02138c42c13668fb1c0bc604b344af58
AirisDom <domstu9@gmail.com>
2024-08-01 14:12:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,21 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -12,20 +27,6 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
@@ -40,16 +41,202 @@ const restaurant = {
       `order recieved ! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`here is your pasta with ${ing1}, ${ing2}, and  ${ing3}`);
+  },
+
+  orderPizza: function (mainIngridients, ...otherIngridients) {
+    console.log(mainIngridients);
+    console.log(otherIngridients);
+  },
 };
 
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
+const properties = object.keys(openingHours);
+console.log(properties);
 
-const newArr = [1, 2, ...arr];
-console.log(newArr);
+let openStr = `We are open on ${properties.length} days: `;
 
-console.log(...newArr);
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+const values = object.values(openingHours);
+console.log(openingHours);
+
+const entries = object.entries(openingHours);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`on ${key} we are open at ${open} and close at ${close}`);
+}
+
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon.open);
+
+// // example
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// for (const day of days) {
+//   console.log(day);
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`on ${day}, we open at ${open}`);
+// }
+
+// // Methods
+// console.log(restaurant.order?.(0, 1) ?? 'method does not exist');
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'method does not exist');
+
+// //arrays
+
+// const users = [{ name: 'jonas', email: 'hello@jonas.com' }];
+
+// console.log(usrs[0]?.name ?? 'user array empty');
+
+// if (users.length > 0) console.log(users[0].name);
+// else console.log('user array empty');
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+// for (const item of menu.entries()) {
+//   console.log(`${item[0]} + 1: ${item[1]}`);
+// }
+
+// console.log(menu.entries());
+
+// const rest1 = {
+//   name: 'capri1',
+//   numGuests: 20,
+// };
+
+// const rest2 = {
+//   name: 'la plaza',
+//   owner: 'giovanni rossi',
+// };
+
+// // rest2.numberGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+// // or assigment operator
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+// // nullish assigment operator (null or undefined)
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+
+// console.log(rest1);
+// console.log(rest2);
+
+// console.log('------------- or -------------');
+// // use any data type return any data type, short-circuiting
+// console.log(3 || 'jonas');
+// console.log('' || 'jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// console.log(undefined || 0 || '' || 'hello' || 23 || null);
+
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// console.log('------------- and ------------------');
+// console.log(0 && 'jonas');
+// console.log(7 && 'jonas');
+
+// console.log('hello' && 23 && null && 'jonas');
+
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+
+// restaurant.orderPizza && restaurant;
+
+// const guestsCorrect = restaurant.numGuests ?? 10;
+// console.log(guestsCorrect);
+
+// const arr = [1, 2, ...[3, 4]];
+
+// // REST ... left side of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// const [pizza, , Risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+
+// console.log(pizza, Risotto, otherFood);
+
+// //objects
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
+
+// // 2) functions
+
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+//   console.log(sum);
+// };
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 674, 6747);
+
+// const x = [23, 5, 7];
+// add(...x);
+
+// restaurant.orderPizza('mushroom', 'onion', 'olives', 'fresh bondziorno');
+// restaurant.orderPizza('mushrooms');
+
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+// console.log(...newArr);
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// const str = 'jonas';
+// const letters = [...str, '', 'S.'];
+// console.log(letters);
+// console.log(...str);
+// // console.log(`${...str}`);
+
+// const ingredients = [
+//   prompt("Let's make pasta! ingridient 1?"),
+//   prompt("Let's make pasta! ingridient 2?"),
+//   prompt("Let's make pasta! ingridient 3?"),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'giuseppe' };
+// console.log(newRestaurant);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
 
 // restaurant.orderDeliver({
 //   time: '22:30',
